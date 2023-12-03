e3267ad211c2c426015e7eccc1632622ccbc06f0
Ashish Sunny <ashishsunny.dev@gmail.com>
2023-12-04 01:23:33 +0530

    Dec 2 | Day 1 Pt:2 | error

--- Day 1/index.mjs
+++ Day 1/index.mjs
@@ -1,6 +1,31 @@
+// Day-1 Pt:1
 import { textArray } from "./data.mjs"
 let val = 0;
-textArray.map((str)=>{
+const substrings = {
+   'one': '1',
+  'two': '2',
+  'three': '3',
+  'four': '4',
+  'five': '5',
+  'six': '6',
+  'seven': '7',
+  'eight': '8',
+  'nine': '9',
+}
+
+function convertStringToNumber(inputString, substring) {
+   const pattern = new RegExp(Object.keys(substring).join('|'), 'g');
+   const resultString = inputString.replace(pattern, match => substring[match]);
+   return resultString;
+ }
+
+const numberedArray = textArray.map((x)=>{
+   return  convertStringToNumber(x, substrings);
+})
+
+
+
+numberedArray.map((str)=>{
    const arrayStr =  str.split('')
    const arr  = []
    arrayStr.map((ele)=>{
@@ -9,7 +34,8 @@ textArray.map((str)=>{
 })
    let num = 0
    num = arr.length===1 ? String(arr[0])+String(arr[0]) :  String(arr[0])+String(arr[arr.length-1])
-   val+=Number(num) 
+   console.log(num)
+   val+=Number(num)
 })
 
 console.log(val)
